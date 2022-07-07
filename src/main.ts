b6c42b7e0b8281423b374a4fac76eb0911bd8965
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-07-07 23:38:04 +0200

    PrimeVue styling added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 
 import PrimeVue from "primevue/config";
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App);
 
