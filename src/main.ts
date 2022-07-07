e84ba575b4c88329a150a710f46f8c29b56f07e4
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-07-07 21:37:46 +0200

    PrimeVue registered

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import PrimeVue from "primevue/config";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+app.use(PrimeVue);
+
+app.mount("#app");
